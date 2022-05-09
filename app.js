f9d6852355bb1749e8024094a4107c6d2891c298
Silvia <silviaj.barrios55@gmail.com>
2022-05-09 08:53:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,79 +1,42 @@
-class Product {
- constructor(Tipo, Dinero, Año) {
-  this.Tipo = Tipo
-  this.Dinero = Dinero
-  this.Año = Año
- }
-}
-var Tipo = ingreso
-if ((Element.Tipo = ingreso)) {
- this.Tipo('success')
-} else {
- this.Tipo('danger')
-}
+var contenedor = document.getElementById('contenedor')
+var Datos = []
 
-class UI {
- addProduct(product) {
-  const productList = document.getElementById('product-list')
-  const element = document.createElement('div')
-  element.innerHTML = `
-               <div class="card text-center mb-4">
-                   <div class="card-body">
-                       <strong>Product</strong>: ${product.Tipo} -
-                       <strong>Dinero</strong>: ${product.Dinero} - 
-                       <strong>Año</strong>: ${product.Año}
-                       <a href="#" class="btn btn-danger" Tipo="delete">Delete</a>
-                   </div>
-               </div>
-           `
-  productList.appendChild(element)
- }
- resetForm() {
-  document.getElementById('product-form').reset()
- }
- deleteProduct(element) {
-  if (element.Tipo === 'delete') {
-   element.parentElement.parentElement.parentElement.remove()
-   this.showMessage('Producto eliminado satisfactoriamente', 'info')
+var Balance = (Datos) => {
+ var Ingresos = 0
+ var Egresos = 0
+ var Balance = document.getElementById('Balance')
+ Datos.forEach((element) => {
+  if (element.Tipo == 'Ingreso') {
+   Ingresos = Ingresos + parseInt(element.Dinero)
+  } else {
+   Egresos = Egresos + parseInt(element.Dinero)
   }
- }
- showMessage(message, cssClass) {
-  const div = document.createElement('div')
-  div.ClassName = 'alert alert-${cssClass} mt-5'
-  div.appendChild(document.createTextNode(message))
-  // mostrando en el DOM
-  const container = document.querySelector('.container')
-  const app = document.querySelector('#App')
-  container.insertBefore(div, app)
-  setTimeout(function () {
-   document.querySelector('.alert').remove()
-  }, 3000)
- }
-}
-// Evento DOM
-document
- .getElementById('product-form')
- .addEventListener('submit', function (e) {
-  e.preventDefault()
-  const Tipo = document.getElementById('Tipo').value,
-   Dinero = document.getElementById('Dinero').value,
-   Año = document.getElementById('Año').value
 
-  const product = new Product(Tipo, Dinero, Año)
-  const ui = new UI()
-
-  if (Tipo === '' || Dinero === '' || Año === '') {
-   return UI.showMessage('Completa los campos')
-  }
-  UI.addProduct(product)
-  UI.showMessage('producto agregado satisfactoriamente', 'success')
-  UI.resetForm()
-
-  e.preventDefault()
+  Balance.innerHTML = `Total: ${Ingresos - Egresos}`
  })
+}
 
-document.getElementById('product-list').addEventListener('click', (e) => {
- const ui = new UI()
- console.log(e.target)
+document.getElementById('product-form').addEventListener('submit', (e) => {
  e.preventDefault()
+ var Tipo = document.getElementById('Tipo').value
+ var Dinero = document.getElementById('Dinero').value
+ var Año = document.getElementById('Año').value
+ var Texto = document.getElementById('Texto').value
+ var dato = { Tipo, Dinero, Año, Texto }
+ Datos.push(dato)
+
+ var nuevo = document.createElement('div')
+ if (Tipo == 'Ingreso') {
+  nuevo.style.background = 'green'
+ } else {
+  nuevo.style.background = 'red'
+ }
+ nuevo.innerHTML = `
+ <p>${Tipo}</p>
+ <p>${Dinero}</p>
+ <p>${Año}</p>
+ <p>${Texto}</p>
+ `
+ contenedor.appendChild(nuevo)
+ Balance(Datos)
 })
